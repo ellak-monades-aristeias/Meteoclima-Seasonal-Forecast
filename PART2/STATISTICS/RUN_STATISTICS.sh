9ec729291ee51bc6915829c31a214f3a8642f089
gvarlas <varlas.giorgos@gmail.com>
2015-09-25 19:39:14 +0300

    Create RUN_STATISTICS.sh

---
+++ PART2/STATISTICS/RUN_STATISTICS.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#set -x
+
+SPATHDIR="path-to-STATISTICS"
+
+cd $SPATHDIR
+
+ii=1
+kk=2
+jj=2
+
+ncl stmm=6 stnn=0 STATYPE=$kk RUN=$jj CASESTUDY=$ii CONTINUOUS.ncl
+ncl stmm=6 stnn=12 STATYPE=$kk RUN=$jj CASESTUDY=$ii CONTINUOUS.ncl
+ncl stmm=6 stnn=24 STATYPE=$kk RUN=$jj CASESTUDY=$ii CONTINUOUS.ncl
+ncl stmm=7 stnn=0 STATYPE=$kk RUN=$jj CASESTUDY=$ii CONTINUOUS.ncl
+ncl stmm=7 stnn=12 STATYPE=$kk RUN=$jj CASESTUDY=$ii CONTINUOUS.ncl
+ncl stmm=7 stnn=24 STATYPE=$kk RUN=$jj CASESTUDY=$ii CONTINUOUS.ncl
+ncl stmm=8 stnn=0 STATYPE=$kk RUN=$jj CASESTUDY=$ii CONTINUOUS.ncl
+ncl stmm=8 stnn=12 STATYPE=$kk RUN=$jj CASESTUDY=$ii CONTINUOUS.ncl
+ncl stmm=8 stnn=24 STATYPE=$kk RUN=$jj CASESTUDY=$ii CONTINUOUS.ncl
